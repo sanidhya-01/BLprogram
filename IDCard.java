1459e10d258fac566c9ec2a50693792a0c6cb6da
sanidhya-01 <sanidhyachauhan2004@gmail.com>
2025-07-14 10:58:26 +0530

    Adding

---
+++ IDCard.java
@@ -0,0 +1,8 @@
+public class IDCard {
+    public static void main(String[]args){
+        System.out.println("Name = Sanidhya Chauhan");
+        System.out.println("Roll NUmber = 2210990786");
+        System.out.println("Phone number = 8307707639");
+        
+    }
+}
